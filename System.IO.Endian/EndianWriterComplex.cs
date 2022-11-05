3498d6295c9c2fbc10420b52ce9d2ffe545c38e5
Gravemind2401 <inferi_sententia@hotmail.com>
2022-11-05 22:56:37 +1100

    simplify xml documentation using <inheritdoc/>

--- System.IO.Endian/EndianWriterComplex.cs
+++ System.IO.Endian/EndianWriterComplex.cs
@@ -11,20 +11,7 @@ namespace System.IO.Endian
     {
         #region WriteObject Overloads
 
-        /// <summary>
-        /// Writes a complex object to the current stream using reflection.
-        /// The type being written must have a public parameterless constructor.
-        /// Each property to be written must have public get/set methods and
-        /// must have at least the <seealso cref="OffsetAttribute"/> attribute applied.
-        /// </summary>
-        /// <typeparam name="T">The type of object to write.</typeparam>
-        /// <param name="value">The object to write.</param>
-        /// <exception cref="AmbiguousMatchException" />
-        /// <exception cref="ArgumentException" />
-        /// <exception cref="ArgumentNullException" />
-        /// <exception cref="InvalidCastException" />
-        /// <exception cref="InvalidOperationException" />
-        /// <exception cref="MissingMethodException" />
+        /// <inheritdoc cref="WriteObject{T}(T, double)"/>
         public void WriteObject<T>(T value)
         {
             if (value == null)
@@ -33,25 +20,8 @@ namespace System.IO.Endian
             WriteObject(value, null);
         }
 
-        /// <summary>
-        /// Writes a complex object to the current stream using reflection.
-        /// The type being written must have a public parameterless constructor.
-        /// Each property to be written must have public get/set methods and
-        /// must have at least the <seealso cref="OffsetAttribute"/> attribute applied.
-        /// </summary>
         /// <typeparam name="T">The type of object to write.</typeparam>
-        /// <param name="value">The object to write.</param>
-        /// <param name="version">
-        /// The version that should be used to store the object.
-        /// This determines which properties will be written, how they will be
-        /// written and at what location in the stream to write them to.
-        /// </param>
-        /// <exception cref="AmbiguousMatchException" />
-        /// <exception cref="ArgumentException" />
-        /// <exception cref="ArgumentNullException" />
-        /// <exception cref="InvalidCastException" />
-        /// <exception cref="InvalidOperationException" />
-        /// <exception cref="MissingMethodException" />
+        /// <inheritdoc cref="WriteObject(object, double)"/>
         public void WriteObject<T>(T value, double version)
         {
             if (value == null)
@@ -60,19 +30,7 @@ namespace System.IO.Endian
             WriteObject(value, (double?)version);
         }
 
-        /// <summary>
-        /// Writes a complex object to the current stream using reflection.
-        /// The type being written must have a public parameterless constructor.
-        /// Each property to be written must have public get/set methods and
-        /// must have at least the <seealso cref="OffsetAttribute"/> attribute applied.
-        /// </summary>
-        /// <param name="value">The object to write.</param>
-        /// <exception cref="AmbiguousMatchException" />
-        /// <exception cref="ArgumentException" />
-        /// <exception cref="ArgumentNullException" />
-        /// <exception cref="InvalidCastException" />
-        /// <exception cref="InvalidOperationException" />
-        /// <exception cref="MissingMethodException" />
+        /// <inheritdoc cref="WriteObject(object, double)"/>
         public void WriteObject(object value)
         {
             if (value == null)
@@ -83,15 +41,19 @@ namespace System.IO.Endian
 
         /// <summary>
         /// Writes a complex object to the current stream using reflection.
+        /// </summary>
+        /// <remarks>
         /// The type being written must have a public parameterless constructor.
         /// Each property to be written must have public get/set methods and
         /// must have at least the <seealso cref="OffsetAttribute"/> attribute applied.
-        /// </summary>
+        /// </remarks>
         /// <param name="value">The object to write.</param>
         /// <param name="version">
         /// The version that should be used to store the object.
+        /// <para>
         /// This determines which properties will be written, how they will be
         /// written and at what location in the stream to write them to.
+        /// </para>
         /// </param>
         /// <exception cref="AmbiguousMatchException" />
         /// <exception cref="ArgumentException" />
