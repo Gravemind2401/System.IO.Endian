2112bbea53c82fba97ddd0005b095824a9384bac
Gravemind2401 <inferi_sententia@hotmail.com>
2023-08-05 11:58:42 +1000

    create classes to define strongly typed structure layouts for dynamic read/write

---
+++ System.IO.Endian/Dynamic/HalfFieldDefinition.cs
@@ -0,0 +1,8 @@
+﻿namespace System.IO.Endian.Dynamic
+{
+    internal class HalfFieldDefinition<TClass> : FieldDefinition<TClass, Half>
+    {
+        protected override Half StreamRead(EndianReader reader, ByteOrder byteOrder) => reader.ReadHalf(byteOrder);
+        protected override void StreamWrite(EndianWriter writer, Half value, ByteOrder byteOrder) => writer.Write(value, byteOrder);
+    }
+}
