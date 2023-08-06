bae1121fcb46d517670ace395d99864dd9950d7a
Gravemind2401 <inferi_sententia@hotmail.com>
2023-08-06 18:37:08 +1000

    dont need HalfFieldDefinition anymore

--- System.IO.Endian/Dynamic/HalfFieldDefinition.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace System.IO.Endian.Dynamic
-{
-    internal class HalfFieldDefinition<TClass> : FieldDefinition<TClass, Half>
-    {
-        protected override Half StreamRead(EndianReader reader, ByteOrder byteOrder) => reader.ReadHalf(byteOrder);
-        protected override void StreamWrite(EndianWriter writer, Half value, ByteOrder byteOrder) => writer.Write(value, byteOrder);
-    }
-}
