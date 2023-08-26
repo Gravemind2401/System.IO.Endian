92722a11cc9b7f51bc350b01bd01781e0dd1c7fb
Gravemind2401 <inferi_sententia@hotmail.com>
2023-08-26 15:11:54 +1000

    use same test structures between read/write tests

---
+++ System.IO.Endian.Tests/Structures/DataLength.cs
@@ -0,0 +1,12 @@
+﻿namespace System.IO.Endian.Tests.Structures
+{
+    public class DataLengthClass01
+    {
+        [Offset(0x00)]
+        public int Property1 { get; set; }
+
+        [DataLength]
+        [Offset(0x04)]
+        public int Property2 { get; set; }
+    }
+}
